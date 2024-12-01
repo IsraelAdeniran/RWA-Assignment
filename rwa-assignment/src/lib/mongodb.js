4bc27b0ff7c883b9b107b033b8f0b0360d75f5f3
IsraelAdeniran <B00157067@mytudublin.ie>
2024-12-01 01:52:03 +0000

    Changes

--- rwa-assignment/src/lib/mongodb.js
+++ rwa-assignment/src/lib/mongodb.js
@@ -6,7 +6,7 @@ async function connectToDatabase() {
     if (!dbConnection) {
         try {
             if (!client) {
-                client = new MongoClient(process.env.DB_URI);
+                client = new MongoClient("mongodb+srv://b00157067:eF7U1qIh5nKLRr1d@cluster1.x3zhu.mongodb.net/RichWeb?retryWrites=true&w=majority&appName=Cluster1");
             }
             await client.connect();
             dbConnection = client.db("RichWeb");
