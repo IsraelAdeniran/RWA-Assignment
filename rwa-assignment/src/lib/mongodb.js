cbe57382eb9f6e26c896e6b65041f53bd18b3a9e
IsraelAdeniran <B00157067@mytudublin.ie>
2024-12-01 01:56:43 +0000

    Changes

--- rwa-assignment/src/lib/mongodb.js
+++ rwa-assignment/src/lib/mongodb.js
@@ -1,15 +1,20 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
+
 let client;
 let dbConnection;
 
 async function connectToDatabase() {
     if (!dbConnection) {
         try {
+            const uri = process.env.MONGODB_URI;
+            if (!uri) {
+                throw new Error("MongoDB URI is not set in environment");
+            }
             if (!client) {
-                client = new MongoClient("mongodb+srv://b00157067:eF7U1qIh5nKLRr1d@cluster1.x3zhu.mongodb.net/RichWeb?retryWrites=true&w=majority&appName=Cluster1");
+                client = new MongoClient(uri);
             }
             await client.connect();
-            dbConnection = client.db("RichWeb");
+            dbConnection = client.db("RichWeb");  // Ensure this matches your actual DB name
         } catch (error) {
             console.error("Database connection error:", error);
             throw error;
